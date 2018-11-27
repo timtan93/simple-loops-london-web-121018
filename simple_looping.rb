35daf83cbc7a0a4294989abcb3127eb2641b09b4
Timothy Tan <timtan93@gmail.com>
2018-11-27 15:46:03 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -9,12 +10,20 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  counter = 1
+  loop do
+    puts phrase
+    break if counter == number_of_times
+    counter += 1
+  end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do
+    puts phrase
+  end
   
 end
 
@@ -22,13 +31,23 @@ def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+ counter = 1 
+ while counter <= number_of_times
+ puts phrase
+ counter +=1 
+ end
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 1 
+  until counter > number_of_times
+  puts phrase
+  counter += 1 
+end
+  
 
 end
 
@@ -37,6 +56,12 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  range = 1..number_of_times
+  for counter in range do
+    puts phrase
+  end
   
 end
+
+
 
